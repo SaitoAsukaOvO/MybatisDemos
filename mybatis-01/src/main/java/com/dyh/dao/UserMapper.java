7dcac47cc364393935127cb58188fdde615a1448
Kyloren96 <Dyhfr96@outlook.com>
2021-02-15 00:31:45 +0800

    Mybatis CRUD操作

--- mybatis-01/src/main/java/com/dyh/dao/UserMapper.java
+++ mybatis-01/src/main/java/com/dyh/dao/UserMapper.java
@@ -6,5 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface UserMapper {
+
+    //獲取全部用戶
     List<User> getUserList();
+
+    //根據id查詢用戶
+    User getUerById(int id);
+
+    //插入用戶
+    int addUser(User user);
+
+    //修改用戶
+    int updateUser(User user);
+
+    //刪除用戶
+    int deleteUser(int id);
 }
--- mybatis-01/src/test/java/com/dyh/dao/UserMapperTest.java
+++ mybatis-01/src/test/java/com/dyh/dao/UserMapperTest.java
@@ -22,4 +22,48 @@ public class UserMapperTest {
 
         sqlSession.close();
     }
+
+    @Test
+    public void getUserById() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.getUerById(1);
+        System.out.println(user);
+
+        sqlSession.close();
+    }
+
+    //增刪改需要提交事務
+    @Test
+    public void addUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        int res = mapper.addUser(new User(4, "H", "333"));
+
+        //commit transaction
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        int res = mapper.updateUser(new User(4, "D", "234"));
+
+        //commit transaction
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        int res = mapper.deleteUser(4);
+
+        //commit transaction
+        sqlSession.commit();
+        sqlSession.close();
+    }
 }
