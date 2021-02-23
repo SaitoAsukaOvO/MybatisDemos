b04ce9fbea7ea5f65e75ac4f5d430f90e9116610
Kyloren96 <Dyhfr96@outlook.com>
2021-02-23 17:34:41 +0800

    分页 rowBounds&resultMap

--- mybatis-004/src/main/java/com/dyh/dao/UserMapper.java
+++ mybatis-004/src/main/java/com/dyh/dao/UserMapper.java
@@ -2,8 +2,16 @@ package com.dyh.dao;
 
 import com.dyh.pojo.User;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserMapper {
     //根據id查詢用戶
     User getUserById(int id);
 
+    //分页1
+    List<User> getUserByLimit(Map<String,Integer> map);
+
+    //分页2
+    List<User> getUserByRowBounds();
 }
--- mybatis-004/src/test/java/com/dyh/dao/UserMapperTest.java
+++ mybatis-004/src/test/java/com/dyh/dao/UserMapperTest.java
@@ -2,9 +2,14 @@ package com.dyh.dao;
 
 import com.dyh.pojo.User;
 import com.dyh.util.MybatisUtils;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.apache.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class UserMapperTest {
@@ -27,6 +32,37 @@ public class UserMapperTest {
         logger.info("info: Enter testlog4j");
         logger.debug("debug: Enter testlog4j");
         logger.error("error: Enter testlog4j");
+    }
+
+    @Test
+    public void getUserByLimit() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("startIndex", 0);
+        map.put("pageSize", 2);
+        List<User> userByLimit = mapper.getUserByLimit(map);
+        for (User user : userByLimit) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void getUserByRowBounds() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //Row Bounds
+        RowBounds rowBounds = new RowBounds(1, 2);
+
+
+        List<User> userList = sqlSession.selectList("com.dyh.dao.UserMapper.getUserByRowBounds", null, rowBounds);
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
     }
 
 }
